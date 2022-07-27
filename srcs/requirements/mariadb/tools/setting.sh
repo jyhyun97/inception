fa23d8e5e90a283c9c73033e793e569dcc9937a4
jeonhyun <jeonhyun@student.42seoul.kr>
2022-07-27 19:38:48 +0900

    wordpreess setting finish

--- srcs/requirements/mariadb/tools/setting.sh
+++ srcs/requirements/mariadb/tools/setting.sh
@@ -10,7 +10,7 @@ service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE";
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'";
 mysql -e "GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER'@'%'";
-# mysql $DB_DATABASE -u root < ./wp_backup.sql
+mysql $DB_DATABASE -u root < ./var/wordpress.sql
 mysql -e "ALTER USER '$DB_ROOT'@'localhost' IDENTIFIED BY '$DB_ROOTPASSWORD'; FLUSH PRIVILEGES;"
 mysqladmin -u$DB_ROOT -p$DB_ROOTPASSWORD shutdown
 
