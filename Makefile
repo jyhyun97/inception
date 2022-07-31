322bae42b468cf6d5749c86c1220ca7f4b8d762f
jeonhyun@student.42seoul.kr <jeonhyun@student.42seoul.kr>
2022-07-31 20:57:32 +0900

    makefile finish

--- Makefile
+++ Makefile
@@ -1,1 +1,27 @@
-test
+CONTAINERS = $(shell sudo docker ps -q)
+IMAGES = $(shell sudo docker images -q)
+VOLUMES = $(shell sudo docker volume ls -q)
+SPACE =
+all :
+	mkdir -p /home/jeonhyun/data
+	mkdir -p /home/jeonhyun/data/wp_db
+	mkdir -p /home/jeonhyun/data/wp_site
+	sudo docker-compose -f srcs/docker-compose.yml up
+up :
+	sudo docker-compose -f srcs/docker-compose.yml up
+down :
+	sudo docker-compose -f srcs/docker-compose.yml down
+
+clean :
+ifneq ($(CONTAINERS), $(SPACE))
+	sudo docker rm -f $(CONTAINERS)
+endif
+ifneq ($(IMAGES), $(SPACE))
+	sudo docker rmi -f $(IMAGES)
+endif
+ifneq ($(VOLUMES), $(SPACE))
+	sudo docker volume rm $(VOLUMES)
+endif
+	sudo rm -rf /home/jeonhyun/data
+
+.PHONY: all down clean
--- srcs/requirements/mariadb/tools/setting.sh
+++ srcs/requirements/mariadb/tools/setting.sh
@@ -1,19 +1,11 @@
 service mysql start;
 
-# mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE}";
-# mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'";
-# mysql -e "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USER}'@'%'";
-# #mysql wordpress -u root < ./example.sql
-# mysql -e "ALTER USER '${DB_ROOT}'@'localhost' IDENTIFIED BY '${DB_ROOTPASSWORD}'; FLUSH PRIVILEGES;"
-# mysqladmin -u${DB_ROOT} -p${DB_ROOTPASSWORD} shutdown
-
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE";
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'";
 mysql -e "GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER'@'%'";
 mysql $DB_DATABASE -u root < ./var/wordpress.sql
 mysql -e "ALTER USER '$DB_ROOT'@'localhost' IDENTIFIED BY '$DB_ROOTPASSWORD'; FLUSH PRIVILEGES;"
 mysqladmin -u$DB_ROOT -p$DB_ROOTPASSWORD shutdown
-
 
 exec mysqld
 
